d8e2dd8d25f28f0664fde540c68d8c9d91c6940e
Unknown <jroremus@gmail.com>
2018-03-02 13:31:46 -0600

    Hello python file updated

--- hello.py
+++ hello.py
@@ -1,9 +1,3 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from app import app2
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-app.run()
+app2.run()
